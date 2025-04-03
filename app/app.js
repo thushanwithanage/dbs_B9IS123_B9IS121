00f0a6a089a888f83c2557458f24ad5f51a30336
thushanwithanage <68364096+thushanwithanage@users.noreply.github.com>
2025-04-03 13:47:01 +0000

    Updated infra file

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Dockerized Node.js app');
+  res.send('Dockerized Node.js app v2');
 });
 
 app.listen(PORT, () => {
