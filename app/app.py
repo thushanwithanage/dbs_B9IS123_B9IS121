4015b46eb74e463081a6466a5d75e84baef1151a
thushanwithanage <68364096+thushanwithanage@users.noreply.github.com>
2025-04-04 13:53:30 +0000

    Updated files

--- app/app.py
+++ app/app.py
@@ -4,6 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"message": "Hello from Python FastAPI on EC2! v1"}
-
-# Note: FastAPI does not use app.run; instead, you run it with an ASGI server like uvicorn.
+    return {"message": "Hello from Python FastAPI on EC2! v2"}
