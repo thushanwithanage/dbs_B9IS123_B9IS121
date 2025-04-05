dac36ae8546bebc19944406f84177020727c2b97
thushanwithanage <68364096+thushanwithanage@users.noreply.github.com>
2025-04-05 18:08:46 +0000

    Updated files

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"message": "Hello from Python FastAPI on EC2 v4"}
+    return {"message": "Hello from Python FastAPI on EC2 v1"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3000)
