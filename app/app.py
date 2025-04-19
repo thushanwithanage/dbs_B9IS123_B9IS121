8e5c3c1167fba5cb1d961de81e3e5ab9f1fb6d5b
thushanwithanage <68364096+thushanwithanage@users.noreply.github.com>
2025-04-19 11:10:57 +0000

    Updated files

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"data": "20058324 Network Systems and Administration assignment"}
+    return {"data": "Thushan Withanage (20058324) Network Systems and Administration assignment"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3000)
